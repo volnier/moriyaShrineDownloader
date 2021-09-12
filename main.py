63eef9e984a1c3ae9d0193d16432ef964ef7644d
Wowiner <39580907+Wowiner@users.noreply.github.com>
2021-09-12 15:45:52 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import json
+import sys
+import requests
+
+
+def main():
+    downloads = {}
+    num = 0
+    with open('downloads.json') as file:
+        oui = json.load(file)
+        for i in oui:
+            downloads[i] = oui[i]
+
+    for i in downloads:
+        num += 1
+        print(str(num) + ". " + i)
+
+    dwnld = input("Which game do you want to download (Enter the name, not the number): ")
+
+    for l in downloads:
+        if dwnld.lower() == l.lower():
+            with open(dwnld.lower(), "wb") as k:
+                print("Downloading %s" % dwnld.lower())
+                response = requests.get(downloads[l], stream=True)
+                length = response.headers.get('content-length')
+                if length is None:  # no length header
+                    k.write(response.content)
+                else:
+                    pp = 0
+                    length = int(length)
+                    for data in response.iter_content(chunk_size=4096):
+                        pp += len(data)
+                        k.write(data)
+                        done = int(50 * pp / length)
+                        sys.stdout.write("\r[%s%s]" % ('X' * done, ' ' * (50 - done)))
+                        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main()
